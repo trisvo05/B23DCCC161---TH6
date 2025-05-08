a2339a1238d6b1c061d47e7169ac7dfbf57314e0
Minh Tri <minhtrivo2005gg@gmail.com>
2025-05-08 19:09:57 +0700

    phuc

--- config/routes.ts
+++ config/routes.ts
@@ -1,92 +1,16 @@
 ﻿export default [
-	{
-		path: '/user',
-		layout: false,
-		routes: [
-			{
-				path: '/user/login',
-				layout: false,
-				name: 'login',
-				component: './user/Login',
-			},
-			{
-				path: '/user',
-				redirect: '/user/login',
-			},
-		],
+	{ path: '/', component: 'index' },
+	{ path: '/plan', component: 'plan' },
+	{ path: '/budget', component: 'budget' },
+	{ 
+	  path: '/admin', 
+	  component: 'admin',
+	  routes: [
+		{ path: '/admin', component: 'admin/index' },
+		{ path: '/admin/destinations', component: 'admin/destinations' },
+		{ path: '/admin/statistics', component: 'admin/statistics' }
+	  ]
 	},
-
-	///////////////////////////////////
-	// DEFAULT MENU
-	{
-		path: '/dashboard',
-		name: 'Dashboard',
-		component: './TrangChu',
-		icon: 'HomeOutlined',
-	},
-	{
-		path: '/gioi-thieu',
-		name: 'About',
-		component: './TienIch/GioiThieu',
-		hideInMenu: true,
-	},
-	{
-		path: '/random-user',
-		name: 'RandomUser',
-		component: './RandomUser',
-		icon: 'ArrowsAltOutlined',
-	},
-
-	// DANH MUC HE THONG
-	// {
-	// 	name: 'DanhMuc',
-	// 	path: '/danh-muc',
-	// 	icon: 'copy',
-	// 	routes: [
-	// 		{
-	// 			name: 'ChucVu',
-	// 			path: 'chuc-vu',
-	// 			component: './DanhMuc/ChucVu',
-	// 		},
-	// 	],
-	// },
-
-	{
-		path: '/notification',
-		routes: [
-			{
-				path: './subscribe',
-				exact: true,
-				component: './ThongBao/Subscribe',
-			},
-			{
-				path: './check',
-				exact: true,
-				component: './ThongBao/Check',
-			},
-			{
-				path: './',
-				exact: true,
-				component: './ThongBao/NotifOneSignal',
-			},
-		],
-		layout: false,
-		hideInMenu: true,
-	},
-	{
-		path: '/',
-	},
-	{
-		path: '/403',
-		component: './exception/403/403Page',
-		layout: false,
-	},
-	{
-		path: '/hold-on',
-		component: './exception/DangCapNhat',
-		layout: false,
-	},
-	{
-		component: './exception/404',
-	},
-];
+	{ path: '*', component: '404' }
+  ];
+  
